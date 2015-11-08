b93067c5c1843e778a4ea16df1d7484489204d39
Daniel O'Brien <wheresmypen@yahoo.com>
2015-11-07 19:33:36 -0700

    locates one updated yelp service

--- server/api/managers/location.manager.js
+++ server/api/managers/location.manager.js
@@ -2,8 +2,23 @@ var services = require('../services');
 
 function LocationManager() {}
 
-LocationManager.prototype.getLocation = function(coords) {
-  return services.yelpService.searchByLocation(coords.latitude, coords.longitude);
+LocationManager.prototype.getLocation = function(coords, radius) {
+  var _this = this;
+  radius = radius || 200;
+  
+  if (!coords) throw new Error('I need coordinates!');
+
+  var yelpPrm = services.yelpService.searchByLocation(coords.latitude, coords.longitude, radius);
+  
+  return yelpPrm.then(function(response) {
+  	console.log(response);
+  	if (response.businesses.length > 0) {
+  	  return response;
+ 	} else {
+      radius += 100;
+      return _this.getLocation(coords, radius);
+  	}
+  });
 }
 
 module.exports = LocationManager;
--- server/api/services/yelp.service.js
+++ server/api/services/yelp.service.js
@@ -6,10 +6,10 @@ function YelpService(config) {
   config = config || {};
 
   this.config = _.extend({
-    consumer_key: process.env.YELP_CONSUMER_KEY, 
-    consumer_secret: process.env.YELP_CONSUMER_SECRET,
-    token: process.env.YELP_TOKEN,
-    token_secret: process.env.YELP_TOKEN_SECRET 
+    consumer_key: 'm1TvMHSF7PoLTIwV85NxbA', 
+    consumer_secret: '6u8JIKkDxYrVWVXmBsLSYPOnb5s',
+    token: '_awxcfRsxMVasHHgxZCjc9w8E_9ytdQe',
+    token_secret: 'g10FIqW_KS8vjIx9dCotKWSRXHU' 
   }, config); 
 
   if (this.config.client) {
@@ -19,11 +19,12 @@ function YelpService(config) {
   }
 }
 
-YelpService.prototype.searchByLocation = function searchByLocation(lat, long) {
+
+
+YelpService.prototype.searchByLocation = function searchByLocation(lat, long, radius) {
   var latlong = lat + ', ' + long;
-  console.log(this.client, latlong);
   var deferred = Q.defer();
-  this.client.search({term: 'restaurants', ll: latlong}, function(error, data) {
+  this.client.search({category_filter: 'restaurants', radius_filter: radius, ll: latlong}, function(error, data) {
     if (error) deferred.reject(error);
     deferred.resolve(data);
   });
